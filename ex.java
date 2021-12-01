5ee80632a1fd09897a29f84b2760d32a45525dd9
LongPeng <longpeng96@qq.com>
2021-12-01 08:28:47 +0800

    Triangle completed

---
+++ ex.java
@@ -0,0 +1,15 @@
+import java.io.FileInputStream;
+import java.util.*;
+import java.io.*;
+public class ex {
+    public static void main(String[] args) throws Exception{
+        FileReader fileReader=new FileReader("reader.txt");
+        FileWriter fileWriter=new FileWriter("writer.txt");
+        int len=0;
+        char[] buff=new char[1024];
+        while(len=fileReader.read(buff)!=-1){
+            f
+        }
+        
+    }
+}
