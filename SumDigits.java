c938d3bbe60362b8c0760e18f435d36595688fbf
LongPeng <longpeng96@qq.com>
2021-12-01 08:41:38 +0800

    completed

---
+++ SumDigits.java
@@ -0,0 +1,13 @@
+public class SumDigits {
+    public int sumDigits(int n) {
+        if(n>0){
+            return n % 10 + sumDigits(n / 10);
+        }
+        else{
+            return 0;
+        }
+    }
+
+    
+}
+
