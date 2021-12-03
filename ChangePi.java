67b12ecaad73961da91fe3fb1f8aca994d85ceda
LongPeng <longpeng96@qq.com>
2021-12-03 15:32:26 +0800

    completed

---
+++ ChangePi.java
@@ -0,0 +1,18 @@
+public class ChangePi {
+    public String changePi(String str) {
+        int ix = str.indexOf("pi");
+        if (ix == -1) {
+            return str;
+        } else {
+            if (ix == 0) {
+                return changePi("3.14" + str.substring(ix + 2));
+            }
+            if (ix == str.length() - 1) {
+                return changePi(str.substring(0, ix + 1) + "3.14");
+            }
+
+            return changePi(str.substring(0, ix) + "3.14" + str.substring(ix + 2));
+        }
+    }
+
+}
