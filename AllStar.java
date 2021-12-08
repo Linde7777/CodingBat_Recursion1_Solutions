315203d54d7098c6dea615bb7eb48274f8aff397
LongPeng <longpeng96@qq.com>
2021-12-08 09:38:23 +0800

    completed

--- AllStar.java
+++ AllStar.java
@@ -1,10 +1,13 @@
 public class AllStar {
     
     public String allStar(String str) {
+        if(str.length()==0){
+            return "";
+        }
         if(str.length()==1){
             return str;
         }
-        return str.substring(0, 1)+"*"+allStar(str.substring(1, str.length()-1));
+        return str.substring(0, 1)+"*"+allStar( str.substring( 1, str.length() ) );
     }
 
 }
