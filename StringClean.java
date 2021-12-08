c693aebe1cc0969b006849a9bdb44b344400b2b9
LongPeng <longpeng96@qq.com>
2021-12-08 15:23:56 +0800

    completed

--- StringClean.java
+++ StringClean.java
@@ -1,13 +1,18 @@
 public class StringClean {
 
-    public String stringClean(String str) {
+    public static void main(String[] args) {
+        String str="aaaa";
+        str=stringClean(str);
+        System.out.println(str);
+    }
+    public static String stringClean(String str) {
         if(str.length()==1){
             return str;
         }
         if(str.substring(0,1).equals(str.substring(1,2))){
-            return str.substring(0,1)+stringClean(str.substring(2));
+            return "" + stringClean(str.substring(0,1)+str.substring(2));
         }else{
-            return str.substring(0,1)+stringClean(str.substring(1));
+            return str.substring(0, 1) + stringClean(str.substring(1));
         }
 
     }
