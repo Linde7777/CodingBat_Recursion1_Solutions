d6aa40ddb6d1cfed95ef929eebd86aee3c050c57
LongPeng <longpeng96@qq.com>
2021-12-08 09:35:27 +0800

    v1

---
+++ AllStar.java
@@ -0,0 +1,10 @@
+public class AllStar {
+    
+    public String allStar(String str) {
+        if(str.length()==1){
+            return str;
+        }
+        return str.substring(0, 1)+"*"+allStar(str.substring(1, str.length()-1));
+    }
+
+}
