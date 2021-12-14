c75d77e5de193ebdc891ea3db22e44b12e5d2505
LongPeng <longpeng96@qq.com>
2021-12-14 14:46:15 +0800

    completed

---
+++ CountHi2.java
@@ -0,0 +1,22 @@
+public class CountHi2 {
+    public static void main(String[] args) {
+        System.out.println(countHi2("xhixhi"));
+    }
+    public static int countHi2(String str) {
+        if(str.length()<=1){
+            return 0;
+        }
+
+        if (str.length() >= 3 && str.substring(0, 3).equals("xhi")) {
+            return 0 + countHi2(str.substring(3));
+        }
+
+        if (str.length() >= 2 && str.substring(0, 2).equals("hi")) {
+            return 1 + countHi2(str.substring(1));
+        } else {
+            return 0 + countHi2(str.substring(1));
+        }
+        
+    }
+
+}
