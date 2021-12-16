2346a5185bc5e5a638265443c18dd3a1d8d0a567
LongPeng <longpeng96@qq.com>
2021-12-16 14:56:29 +0800

    completed

--- StrDist.java
+++ StrDist.java
@@ -1,14 +1,20 @@
 public class StrDist {
-    public int strDist(String str, String sub) {
+    public static int strDist(String str, String sub) {
         if(str.length()<sub.length()){
             return 0;
         }
+
         if(sub.equals(str.substring(0,sub.length()))){
-            if(str.lastIndexOf(sub)!=-1){
-                return 2 * sub.length() + str.substring(sub.length(), str.lastIndexOf(sub)).length();
-            }else{
+            if(str.lastIndexOf(sub)==0){
                 return sub.length();
             }
+            else{
+                return 0+str.lastIndexOf(sub)+sub.length();
+                /* 0 is the index of the first occurrence of sub */
+                /* e.g. strDist("hihihi","hih") */
+                
+            }
+
         }else{
             return 0+strDist(str.substring(1), sub);
         }
